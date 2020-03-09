a6c3698b91fa26782ba06d043b693b16df59ae87
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-09 20:09:41 +0200

    set mapping to controller

--- src/main/java/com/psy888/fcc/api/PersonController.java
+++ src/main/java/com/psy888/fcc/api/PersonController.java
@@ -3,14 +3,21 @@ package com.psy888.fcc.api;
 import com.psy888.fcc.model.Person;
 import com.psy888.fcc.service.PersonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RequiredArgsConstructor
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
+
     private final PersonService personService;
 
-    public void addPerson(Person person){
+    @PostMapping
+    public void addPerson(@RequestBody Person person){
         personService.addPerson(person);
     }
 }
