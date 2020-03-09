639d88f45b9ecab715d39fbaeef15acd778a4969
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-09 19:38:36 +0200

    dao impl

--- src/main/java/com/psy888/fcc/dao/FakePersonDataAccessService.java
+++ src/main/java/com/psy888/fcc/dao/FakePersonDataAccessService.java
@@ -2,11 +2,17 @@ package com.psy888.fcc.dao;
 
 import com.psy888.fcc.model.Person;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class FakePersonDataAccessService implements PersonDao {
+
+    private static List<Person> DB = new ArrayList<>();
+
     @Override
     public int insertPerson(UUID id, Person person) {
-        return 0;
+        DB.add(new Person(id,person.getName()));
+        return 1;
     }
 }
