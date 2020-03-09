f41540ba2814dd6f8aa115801351634826fece57
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-09 23:36:34 +0200

    add db interaction

--- src/main/java/com/psy888/fcc/dao/PersonDataAccessService.java
+++ src/main/java/com/psy888/fcc/dao/PersonDataAccessService.java
@@ -1,13 +1,20 @@
 package com.psy888.fcc.dao;
 
 import com.psy888.fcc.model.Person;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 @Repository("postgres")
+@RequiredArgsConstructor
 public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -15,7 +22,12 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public List<Person> selectAllPerson() {
-        return List.of(new Person(UUID.randomUUID(),"FROM POSTGRES DB"));
+        String sql = "SELECT id, name FROM person;";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
