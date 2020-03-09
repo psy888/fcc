b28d1fa7d424fc9146c613f70f6aa853a73ec694
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-09 19:23:20 +0200

    init

---
+++ src/test/java/com/psy888/fcc/FccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psy888.fcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
