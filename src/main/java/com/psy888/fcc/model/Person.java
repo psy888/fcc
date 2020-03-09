2fdb95db6d78ae068b7edae89b4a72e97ba22cc0
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-09 20:03:43 +0200

    add json property name to object fields

--- src/main/java/com/psy888/fcc/model/Person.java
+++ src/main/java/com/psy888/fcc/model/Person.java
@@ -1,10 +1,13 @@
 package com.psy888.fcc.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 import java.util.UUID;
 @Value
 public class Person {
-    private final UUID id;
-    private final String name;
+    @JsonProperty("id")
+    UUID id;
+    @JsonProperty("name")
+    String name;
 }
